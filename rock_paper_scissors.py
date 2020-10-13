6639e391ac378bd3eee53876bc382630aed047b7
Obinna Igwe <igwenus619@gmail.com>
2020-10-13 14:48:45 +0100

    Check for win, loss and draw conditions

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,7 +1,40 @@
+import random as r
+
+
+def get_computer_choice():
+    options = ["rock", "paper", "scissors"]
+    return r.choice(options)
+
+
+def determine_verdict_v2(user_choice, computer_choice):
+    win_conditions = {"rock": "paper", "paper": "scissors", "scissors": "rock"}
+    win_condition = win_conditions[computer_choice]
+    if user_choice == computer_choice:
+        return f"There is a draw ({computer_choice})"
+    elif user_choice == win_condition:
+        return f"Well done. The computer chose {computer_choice} and failed"
+    else:
+        return f"Sorry, but the computer chose {computer_choice}"
+
+
+def determine_verdict(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        return f"There is a draw ({computer_choice})"
+    elif computer_choice == "rock":
+        if user_choice == "paper":
+            return f"Well done. The computer chose {computer_choice} and failed"
+        return f"Sorry, but the computer chose {computer_choice}"
+    elif computer_choice == "paper":
+        if user_choice == "scissors":
+            return f"Well done. The computer chose {computer_choice} and failed"
+        return f"Sorry, but the computer chose {computer_choice}"
+    elif computer_choice == "scissors":
+        if user_choice == "rock":
+            return f"Well done. The computer chose {computer_choice} and failed"
+        return f"Sorry, but the computer chose {computer_choice}"
+
+
 user_choice = input()
-if user_choice == "rock":
-    print("Sorry, but the computer chose paper")
-elif user_choice == "paper":
-    print("Sorry, but the computer chose scissors")
-elif user_choice == "scissors":
-    print("Sorry, but the computer chose rock")
+computer_choice = get_computer_choice()
+verdict = determine_verdict_v2(user_choice, "paper")
+print(verdict)
