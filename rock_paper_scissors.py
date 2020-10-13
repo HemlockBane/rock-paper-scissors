7c4df6190d179096f2eb93ac5a5cc2c56615f3c3
Obinna Igwe <igwenus619@gmail.com>
2020-10-13 11:16:33 +0100

    Initial commit

---
+++ rock_paper_scissors.py
@@ -0,0 +1,7 @@
+user_choice = input()
+if user_choice == "rock":
+    print("Sorry, but the computer chose paper")
+elif user_choice == "paper":
+    print("Sorry, but the computer chose scissors")
+elif user_choice == "scissors":
+    print("Sorry, but the computer chose rock")
