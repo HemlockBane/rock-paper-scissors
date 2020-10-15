7ab58c030109c27a3739772946b44e47de559671
Obinna Igwe <igwenus619@gmail.com>
2020-10-15 23:50:46 +0100

    Allow user to select more options

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -4,20 +4,37 @@ import random as r
 class RockPaperScissors:
     def __init__(self):
         self.rating = 0
+        self.game_options = ["rock", "paper", "scissors"]
 
     def get_computer_choice(self):
         options = ["rock", "paper", "scissors"]
         r.seed()
         return r.choice(options)
 
+    def shift(self, diff):
+        if diff < 0:
+            diff = -diff
+            return self.game_options[diff:] + self.game_options[:diff]  # left shift
+        elif diff > 0:
+            return self.game_options[-diff:] + self.game_options[:-diff]  # right shift
+
     def determine_verdict_v2(self, user_choice, computer_choice):
-        win_conditions = {"rock": "paper",
-                          "paper": "scissors", "scissors": "rock"}
-        win_condition = win_conditions[computer_choice]
-        if user_choice == computer_choice:
+        mid_idx = len(self.game_options)//2  # middle of array
+        user_input_idx = self.game_options.index(user_choice)
+        diff = mid_idx - user_input_idx
+        if diff != 0:
+            self.game_options = self.shift(diff)
+
+        loss_conditions = self.game_options[:mid_idx]
+        win_conditions = self.game_options[mid_idx + 1:]
+        # print(self.game_options)
+        # print(loss_conditions)
+        # print(win_conditions)
+
+        if computer_choice == user_choice:
             self.update_rating(50)
             return f"There is a draw ({computer_choice})"
-        elif user_choice == win_condition:
+        elif computer_choice not in win_conditions:
             self.update_rating(100)
             return f"Well done. The computer chose {computer_choice} and failed"
         else:
@@ -59,14 +76,20 @@ class RockPaperScissors:
 
         self.init_rating(user_name)
 
+        user_game_options = input()
+        if len(user_game_options) != 0:
+            self.game_options = user_game_options.split(",")
+
+        print("Okay, let's start")
         while True:
             user_choice = input().lower()
+
             if user_choice == "!exit":
                 print("Bye!")
                 break
             elif user_choice == "!rating":
                 self.display_rating()
-            elif user_choice in ["rock", "paper", "scissors"]:
+            elif user_choice in self.game_options:
                 computer_choice = self.get_computer_choice()
                 verdict = self.determine_verdict_v2(
                     user_choice, computer_choice)
